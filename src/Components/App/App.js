8ce6bf3a6ae7c680b02617d5f3dc29f0b49bab92
Naomi Campos <naomicampos@nkcampos.local>
2020-01-11 11:18:42 -0700

    Add router to get to single beer card page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import BeerCardContainer from '../../Containers/BeerCardContainer/BeerCardContainer'
 import './App.css';
+import { Route } from 'react-router-dom';
+import BeerCard from '../BeerCard/BeerCard';
+import { connect } from 'react-redux'
 
-function App() {
+export const App = ({ beers }) => {
   return (
     <div className="App">
      <h1>CRAFT</h1>
-     <BeerCardContainer />
+     <Route exact path='/' component={BeerCardContainer} />
+     <Route path='/beer/:id' render={({ match }) => {
+       const beer = beers.find(beer => beer.id === +(match.params.id));
+       return <BeerCard {...beer}/>
+     }} />
     </div>
   );
 }
 
-export default App;
+export const mapStateToProps = state => ({
+  beers: state.beers
+})
+
+export default connect(mapStateToProps)(App);
