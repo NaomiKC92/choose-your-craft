3eb6a6846f54521720736a9550c39dbcac81736d
Naomi Campos <naomicampos@nkcampos.local>
2020-01-13 06:19:01 -0700

    Complete test setup

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />)
+    console.log(wrapper)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('mapStateToProps', () => {
+    it('should return an object with the beers array', () => {
+      const mockState = {
+        beers: [{name: 'Trashy Blond', abv: 4.2}]
+      }
+
+
+    });
+  });
+
 });
